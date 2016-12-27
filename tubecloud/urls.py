f4624086368681949ad3cc154ec56589c1457c81
Keita <likenirvana@me.com>
2016-12-28 02:57:21 +0900

    retry

--- tubecloud/urls.py
+++ tubecloud/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include, url
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/$', admin.site.urls),
     url(r'^keijiban/', include('keijiban.urls')),
 ]
