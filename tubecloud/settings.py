1c8f2d6b2f8c3f35dd3ce956469cc7323c8ec8d2
Keita <likenirvana@me.com>
2016-12-28 04:01:10 +0900

    retry

--- tubecloud/settings.py
+++ tubecloud/settings.py
@@ -161,5 +161,5 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(PROJECT_ROOT, "static"), #PROJECT_ROOTはBASE_DIRだった
 )
