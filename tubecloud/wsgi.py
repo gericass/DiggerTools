638b5dda4323eebaed47dd08d13e69e3acf2c9e1
Keita <likenirvana@me.com>
2016-12-28 16:33:50 +0900

    retry

--- tubecloud/wsgi.py
+++ tubecloud/wsgi.py
@@ -21,21 +21,16 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tubecloud.settings")
 
 application = Cling(get_wsgi_application())
 #application = DjangoWhiteNoise(application)
-
+'''
 
 
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tubecloud.settings")
 
 application = get_wsgi_application()
-'''
 
-
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
-application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
