9eff7f6f2729c7647cc751b8691e1e6c663cbd59
Keita <likenirvana@me.com>
2016-12-28 14:19:08 +0900

    retry

--- tubecloud/wsgi.py
+++ tubecloud/wsgi.py
@@ -11,8 +11,9 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 import os
-
 from django.core.wsgi import get_wsgi_application
+
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tubecloud.settings")
 
