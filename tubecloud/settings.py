38d53d97b06a474ac0c9408708205d93bd72d884
Keita <likenirvana@me.com>
2017-01-06 14:24:07 +0900

    retry

--- tubecloud/settings.py
+++ tubecloud/settings.py
@@ -172,5 +172,5 @@ STATICFILES_DIRS = (
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #追加した
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 #-------------
