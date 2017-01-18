fcb2d1059566685016204dea3b8000c558f1c394
Keita <likenirvana@me.com>
2017-01-18 15:22:11 +0900

    retry

--- tubecloud/settings.py
+++ tubecloud/settings.py
@@ -161,6 +161,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),  # PROJECT_ROOTはBASE_DIRだった
+    os.path.join(BASE_DIR, 'templates'),  # BASE_DIRだった
 )
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
