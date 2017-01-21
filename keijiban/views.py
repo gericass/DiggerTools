cd9a845d94358cb91ec1154dea4554626f2acff0
Keita <likenirvana@me.com>
2017-01-21 17:38:33 +0900

    retry

--- keijiban/views.py
+++ keijiban/views.py
@@ -21,9 +21,9 @@ def kakikomi(request):
       #link = get_template('results.html')
       #return link.render(request)#,{'form2':tweets}) #{'id':f}
       if q==0:
-       return HttpResponseRedirect('/results/')
+       return HttpResponseRedirect('/keijiban/results/')
       else:
-        return HttpResponseRedirect('/ineeyo/')
+        return HttpResponseRedirect('/keijiban/ineeyo/')
   else:
      f = idkakikomi()
 
