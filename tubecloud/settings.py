5c2866de9ac122bd0867a6477147b3acb8a297f4
Keita <likenirvana@me.com>
2017-02-21 16:52:45 +0900

    retry

--- tubecloud/settings.py
+++ tubecloud/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'keijiban',
-    #'gunicorn',
+    'gunicorn',
 ]
 
 MIDDLEWARE = [
@@ -68,7 +68,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(OP_DIR, 'templates'),  # BASE_DIRだった
+            #os.path.join(OP_DIR, 'templates'),  # BASE_DIRだった
             #os.path.join(BASE_DIR, "static"),  # BASE_DIR
         ],
         'APP_DIRS': True,
