d74a53a420c1eee0f68d59d3cf8f520a648db3d3
Keita <bonjovi1926@yahoo.co.jp>
2017-09-10 12:37:44 +0900

    fix: resident_process

--- keijiban/bach_script.py
+++ keijiban/bach_script.py
@@ -2,8 +2,8 @@ import requests
 import time
 
 def access():
+    print("logged...")
     while True:
         requests.get("https://salty-lake-16271.herokuapp.com/")
         print("Logged!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         time.sleep(10)
-    print("logged...")
