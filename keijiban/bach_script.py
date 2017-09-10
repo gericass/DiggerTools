d0e7f04b5caabd005d624be84e36d2a5a3dc3212
Keita <bonjovi1926@yahoo.co.jp>
2017-09-10 12:50:13 +0900

    fix

--- keijiban/bach_script.py
+++ keijiban/bach_script.py
@@ -3,7 +3,10 @@ import time
 
 def access():
     print("logged...")
+    cnt = 0
     while True:
+        cnt += 1
         requests.get("https://salty-lake-16271.herokuapp.com/")
         print("Logged!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print(cnt)
         time.sleep(10)
