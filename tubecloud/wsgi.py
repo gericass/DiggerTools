009b85e18cb53833e2800f10a0c25541ef743ed2
Keita <bonjovi1926@yahoo.co.jp>
2017-09-10 12:20:17 +0900

    fix: wsgi

--- tubecloud/wsgi.py
+++ tubecloud/wsgi.py
@@ -39,6 +39,8 @@ from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
 
+'''
 while True:
     logger.access()
     time.sleep(10)
+'''
